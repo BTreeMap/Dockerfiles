c11b43a8a0671c1443e7e00133dbb489a25622ea
Joe Fang <25814618+MinecraftFuns@users.noreply.github.com>
2024-11-25 23:30:00 -0500

    fix: keep Docker container running
    Reference: https://stackoverflow.com/questions/25775266/how-to-keep-docker-container-running-after-starting-services
    
    run-ci

--- tailscale-over-cloudflare-warp-arm64/entrypoint.sh
+++ tailscale-over-cloudflare-warp-arm64/entrypoint.sh
@@ -15,24 +15,4 @@ dbus-daemon --config-file=/usr/share/dbus-1/system.conf
 # running it in the foreground
 warp-svc --accept-tos &
 
-# sleep to wait for the daemon to start, default 2 seconds
-sleep "$WARP_SLEEP"
-
-# if /var/lib/cloudflare-warp/reg.json not exists, setup new warp client
-if [ ! -f /var/lib/cloudflare-warp/reg.json ]; then
-    # if /var/lib/cloudflare-warp/mdm.xml not exists or REGISTER_WHEN_MDM_EXISTS not empty, register the warp client
-    if [ ! -f /var/lib/cloudflare-warp/mdm.xml ] || [ -n "$REGISTER_WHEN_MDM_EXISTS" ]; then
-        warp-cli registration new && echo "Warp client registered!"
-        # if a license key is provided, register the license
-        if [ -n "$WARP_LICENSE_KEY" ]; then
-            echo "License key found, registering license..."
-            warp-cli registration license "$WARP_LICENSE_KEY" && echo "Warp license registered!"
-        fi
-    fi
-    # connect to the warp server
-    warp-cli --accept-tos connect
-else
-    echo "Warp client already registered, skip registration"
-fi
-
-warp-cli debug qlog disable
+exec tail -f /dev/null
