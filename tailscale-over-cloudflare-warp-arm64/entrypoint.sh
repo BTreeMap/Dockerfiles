38d48261d22f76f6f6cc3abcffc1e0414362bca8
Joe Fang <25814618+MinecraftFuns@users.noreply.github.com>
2024-11-25 21:42:27 -0500

    fix: temoprarily use root user inside the container
    References:
    
    https://stackoverflow.com/questions/68155641/should-i-run-things-inside-a-docker-container-as-non-root-for-safety
    
    https://www.redhat.com/en/blog/understanding-root-inside-and-outside-container
    
    run-ci

--- tailscale-over-cloudflare-warp-arm64/entrypoint.sh
+++ tailscale-over-cloudflare-warp-arm64/entrypoint.sh
@@ -1,14 +1,14 @@
 # Create the /run/dbus directory if it does not already exist
-sudo mkdir -p /run/dbus
+mkdir -p /run/dbus
 
 # Check if the D-Bus PID file exists; if it does, remove it
 if [ -f /run/dbus/pid ]; then
-    sudo rm /run/dbus/pid
+    rm /run/dbus/pid
 fi
 
 # Start the D-Bus daemon with the specified configuration file
-sudo dbus-daemon --config-file=/usr/share/dbus-1/system.conf
+dbus-daemon --config-file=/usr/share/dbus-1/system.conf
 
 # Start the warp service daemon with the accept terms of service option,
 # running it in the background
-sudo warp-svc --accept-tos &
+warp-svc --accept-tos &
