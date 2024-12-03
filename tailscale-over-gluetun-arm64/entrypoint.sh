eff8549bd8f6def26c7b5f2f0b6535d583bcd3ae
Joe Fang <25814618+MinecraftFuns@users.noreply.github.com>
2024-12-02 21:02:08 -0500

    fix: entrypoint
    run-ci

--- tailscale-over-gluetun-arm64/entrypoint.sh
+++ tailscale-over-gluetun-arm64/entrypoint.sh
@@ -5,31 +5,34 @@
 #   - net_admin: Allows the container to manage networking aspects.
 #   - sys_module: Enables loading and unloading kernel modules.
 
-# Exit the script immediately if any command fails
+# Exit the script immediately if any command fails.
 set -e
 
-# Optimize Tailscale performance by configuring UDP generic receive offload (GRO)
+# Optimize Tailscale performance by enabling UDP Generic Receive Offload (GRO).
 echo "Optimizing Tailscale performance with UDP GRO settings..."
+# Get the network device used for the default route.
 NETDEV=$(ip -o route get 8.8.8.8 | cut -f 5 -d " ")
+# Enable UDP GRO forwarding and disable GRO for other protocols.
 ethtool -K $NETDEV rx-udp-gro-forwarding on rx-gro-list off
 
-# Start the Tailscale daemon in the background
+# Start the Tailscale daemon in the background.
 echo "Starting the Tailscale daemon..."
 tailscaled --statedir=/var/lib/tailscale &
 
-# Sleep for 5 seconds to allow Tailscale daemon to initialize
+# Sleep for 5 seconds to allow the Tailscale daemon to initialize.
 echo "Sleeping for 5 seconds to allow the Tailscale daemon to initialize..."
 sleep 5
 
-# Bring up the Tailscale connection and advertise this node as an exit node
+# Bring up the Tailscale connection and advertise this node as an exit node.
 echo "Connecting Tailscale and advertising this node as an exit node..."
 tailscale up --advertise-exit-node
 
-# Allow some time (5 seconds) for the Tailscale connection to establish
+# Allow some time (5 seconds) for the Tailscale connection to establish.
 echo "Sleeping for 5 seconds to allow Tailscale connection to establish..."
 sleep 5
 
+# Start the Gluetun entrypoint script in the background.
 ./gluetun-entrypoint &
 
+# Keep the script running indefinitely to prevent container exit.
 exec tail -f /dev/null
-
