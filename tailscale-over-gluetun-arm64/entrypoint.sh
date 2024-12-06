ac711f4ff9299ed70bf56a000e1c094611370801
Joe Fang <25814618+MinecraftFuns@users.noreply.github.com>
2024-12-06 13:35:56 -0500

    feat: install tailscale from docker.io

--- tailscale-over-gluetun-arm64/entrypoint.sh
+++ tailscale-over-gluetun-arm64/entrypoint.sh
@@ -28,7 +28,7 @@ ethtool -K $NETDEV rx-udp-gro-forwarding on rx-gro-list off
 
 # Start the Tailscale daemon in the background.
 echo "Starting the Tailscale daemon..."
-tailscaled --statedir=/var/lib/tailscale &
+/app/tailscaled --statedir=/var/lib/tailscale &
 
 # Sleep for 5 seconds to allow the Tailscale daemon to initialize.
 echo "Sleeping for 5 seconds to allow the Tailscale daemon to initialize..."
@@ -36,7 +36,7 @@ sleep 5
 
 # Bring up the Tailscale connection and advertise this node as an exit node.
 echo "Connecting Tailscale and advertising this node as an exit node..."
-tailscale up --advertise-exit-node --accept-dns=false
+/app/tailscale up --advertise-exit-node --accept-dns=false --webclient
 
 # Allow some time (5 seconds) for the Tailscale connection to establish.
 echo "Sleeping for 5 seconds to allow Tailscale connection to establish..."
